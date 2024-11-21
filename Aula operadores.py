90d560c48d7f8f6cf644a9a627a31103fd4bd38f
Hadassa Honorato Próspero <hadassahonorato77@gmail.com>
2024-11-21 15:14:43 -0300

    Entregando exercícios com operadores

---
+++ Aula operadores.py
@@ -0,0 +1,48 @@
+print("Olá mundo")
+print (6+4*2)
+print (4*9+5)
+print ((6+7)*9)
+print (15+20-30)
+print (6+5**2)
+''''''
+#soma
+soma = 5+5
+
+#subtração 
+sub = 4-3
+
+#multiplicação 
+multi = 3*3
+
+#divisão 
+div = 3/2
+
+#divisão inteira
+div2 = 3 // 2
+
+#potencia
+pot = 4 ** 2 
+
+#modulo 
+mod = 25 %  2 
+
+print (div)
+#Descobrir tipo de dado da variavel 
+print (type(div))
+
+#Conversão de dados 
+numl = float(input ("Digite o primeiro numero:"))
+num2 = float(input ("Digite o segundo numero:"))
+soma = numl + num2
+print ("A soma dos numeros e: " , soma)
+
+#f-string
+nome="Hadassa"
+sobrenome="Honorato"
+print(f"Meu nome é {nome} e meu sobrenome é {sobrenome}")
+''''''
+####################################################
+
+PI = 3.14159265
+print(f"O valor de PI é: {PI:.2f}")
+
---
+++ Exercício 1.py
@@ -0,0 +1,8 @@
+num1 = float(input("Didite o primeiro numero"))
+num2 = float (input("Digite o segundo numero"))
+soma = (num1 + num2)
+print (f"A soma é {soma}")
+mult = (num1*num2)
+print (f"A mult e {mult}")
+div= (num1/num2)
+print (f"A divisao é {div}")
